4de3059a56547bc71d31c3ca9408ef9f5d326d50
DylanAbeyta <dylanabeyta@gmail.com>
2018-11-29 15:03:20 -0700

    submitting the final version of the Character making porgram

--- Character_DylanAbeyta/Character_DylanAbeyta/Character.cs
+++ Character_DylanAbeyta/Character_DylanAbeyta/Character.cs
@@ -8,5 +8,19 @@ namespace Character_DylanAbeyta
 {
     class Character
     {
+        public string characterName;
+        public int age;
+        public string eyeColor;
+        public float height;
+        public string hairColor;
+
+        public Character(string characterName, int age, string eyeColor, float height, string hairColor)
+        {
+            this.characterName = characterName;
+            this.age = age;
+            this.eyeColor = eyeColor;
+            this.height = height;
+            this.hairColor = hairColor; 
+        }
     }
 }
--- Character_DylanAbeyta/Character_DylanAbeyta/Program.cs
+++ Character_DylanAbeyta/Character_DylanAbeyta/Program.cs
@@ -21,80 +21,100 @@ namespace Character_DylanAbeyta
         static int a;
         static float height;
         static float he;
-
+        static List<Character> character = new List<Character>();
 
         static void Main(string[] args)
         {
-            List<Character> character = new List<Character>();
             do
             {
+                Console.WriteLine("*********************************");
                 Console.WriteLine("1) create a character");
                 Console.WriteLine("2) modify a character");
                 Console.WriteLine("3) delete a character");
                 Console.WriteLine("4) load existing characters");
                 Console.WriteLine("5) list characters");
                 Console.WriteLine("6) exit");
+                Console.WriteLine("*********************************");
                 input = int.Parse(Console.ReadLine());
 
                 if (input == 1)
                 {
                     character.Add(CreateCharacter());
-                    SaveCharacter(character);
                 }
 
                 if (input == 2)
                 {
-                    characterName = ModifyCharacter();
+                    Console.WriteLine("What Character Would you like to modify?");
+                    string filename = Console.ReadLine();
+                    character.Add(LoadCharacter(filename));
+                    for (int i = 0; i < character.Count; i++)
+                    {
+                        if (character[i].characterName == filename)
+                        {
+                            ModifyCharacter(character[i]);
+                            break;
+                        }
+                        else
+                            Console.WriteLine("file not found");
+                    }
                 }
 
                 if (input == 3)
                 {
-                    DeleteCharacter();
+                    if (DeleteCharacter(character, characterName) == true)
+                    {
+                        Console.WriteLine(characterName + "was deleted");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine(characterName + "does not exist, therefore was not removed");
+                    }
                 }
 
                 if (input == 4)
                 {
-                    LoadCharacter();
+                    Console.WriteLine("what character would you like to load?");
+                    string uname = Console.ReadLine();
+                    character.Add(LoadCharacter(uname));
                 }
 
                 if (input == 5)
-                {
-                    ListCharacter();
-                }   
+                {                  
+                    ListCharacter(character);
+                }
 
             } while (input != 6);
-            string eyeColor;
-
-            Console.WriteLine("what color is you eye? ");
+            Console.ReadKey();
         }
 
         static Character CreateCharacter()
         {
-            Console.WriteLine("what is your age?");
-            age = int.Parse(Console.ReadLine());
             Console.WriteLine("what is your name?");
             name = Console.ReadLine();
+            Console.WriteLine("what is your age?");
+            age = int.Parse(Console.ReadLine());
             Console.WriteLine("what is your eye color?");
             eyeColor = Console.ReadLine();
             Console.WriteLine("what is your height?");
             height = float.Parse(Console.ReadLine());
             Console.WriteLine("what is your hair color?");
             hairColor = Console.ReadLine();
-
+            Character character = new Character(name, age, eyeColor, height, hairColor);
+            SaveCharacter(character);
             return character;
         }
 
         static void ModifyCharacter(Character character)
-        {
+        {            
             h = hairColor;
             n = name;
             e = eyeColor;
             a = age;
-            he = height;
+            he = height;           
 
             Console.WriteLine("what Characteristic would you like to modify?");
             Console.WriteLine("h = hairColor");
-            Console.WriteLine("n = name");
             Console.WriteLine("e = eyeColor");
             Console.WriteLine("a = age");
             Console.WriteLine("he = height");
@@ -104,64 +124,90 @@ namespace Character_DylanAbeyta
             {
                 Console.WriteLine("what is your hair color?");
                 hairColor = Console.ReadLine();
-            }
-
-            if (modify == "n")
-            {
-                Console.WriteLine("what is your name?");
-                name = Console.ReadLine();
-            }
+                character.hairColor = hairColor;
+            }         
 
             if (modify == "e")
             {
                 Console.WriteLine("what is your eye color?");
                 eyeColor = Console.ReadLine();
+                character.eyeColor = eyeColor;
             }
 
             if (modify == "a")
             {
                 Console.WriteLine("what is your age?");
                 age = int.Parse(Console.ReadLine());
+                character.age = age;
             }
-
+     
             if (modify == "he")
             {
                 Console.WriteLine("what is your height?");
                 height = float.Parse(Console.ReadLine());
+                character.height = height;
             }
-
-
-
-
+            SaveCharacter(character);
         }
 
         static void SaveCharacter(Character character)
         {
-            string path = AppDomain.CurrentDomain.BaseDirectory + @"character." + characterName + ".txt";
+            Console.WriteLine("what character would you like to save?");
+            string characterName = Console.ReadLine();
+            string path = AppDomain.CurrentDomain.BaseDirectory + @"character." + character.characterName + ".txt";
 
             using (StreamWriter sw = new StreamWriter(path))
             {
-                sw.WriteLine(character);
+                sw.WriteLine(character.characterName + "\r\n" + character.age + "\r\n" + character.eyeColor + "\r\n" + character.height + "\r\n" + character.eyeColor);
             }
         }
 
         static bool DeleteCharacter(List<Character> characters, string characterName)
         {
+            bool Deleted = false;
+            string delete;
+            Console.WriteLine("what character would you like to delete?");
+            delete = Console.ReadLine();
 
-        }
-        static List<Character> LoadCharacters()
-        {
-            return ;
+            if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + @"character." + delete + ".txt"))
+            {
+                File.Delete(AppDomain.CurrentDomain.BaseDirectory + @"character." + delete + ".txt");
+                Deleted = true;
+            }
+            else
+            {
+                Deleted = false;
+            }
+
+            return Deleted;
         }
 
         static Character LoadCharacter(string characterName)
         {
-            return ;
+            string path = AppDomain.CurrentDomain.BaseDirectory + @"character." + characterName + ".txt";
+            string line;
+            List<String> file = new List<String>();
+            using (StreamReader sr = new StreamReader(path))
+            {              
+                while ((line = sr.ReadLine()) != null)
+                {
+                    file.Add(line);
+                }                           
+            }
+            Character NCharacter = new Character(file[0], int.Parse(file[1]), file[2], float.Parse(file[3]), file[4]);
+            return NCharacter;
         }
 
-        static void ListCharacter(List<Character> characters)
-        {
-
+        static void ListCharacter(List<Character> character)
+        {                     
+            for (int i = 0; i < character.Count; i++)
+            {
+                Console.WriteLine(character[i].characterName);
+                Console.WriteLine(character[i].age);
+                Console.WriteLine(character[i].eyeColor);
+                Console.WriteLine(character[i].height);
+                Console.WriteLine(character[i].hairColor);
+            }                                  
         }
 
     }
