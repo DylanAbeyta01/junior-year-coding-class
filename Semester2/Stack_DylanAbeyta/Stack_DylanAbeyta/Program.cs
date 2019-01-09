5dc4cd5b7f5070488bfb79bcf1ff80177dfa2bbc
DylanAbeyta <dylanabeyta@gmail.com>
2019-01-09 15:08:21 -0700

    i made a semester 2 file and made the stack program in it

---
+++ Semester2/Stack_DylanAbeyta/Stack_DylanAbeyta/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_DylanAbeyta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack myStack = new Stack();
+            
+            int input;
+            do
+            {
+                Console.WriteLine("***************");
+                Console.WriteLine("** 1) Push");
+                Console.WriteLine("** 2) Pop");
+                Console.WriteLine("** 3) Peek");
+                Console.WriteLine("** 4) Print");
+                Console.WriteLine("** 5) search");
+                Console.WriteLine("** 6) quit");
+                Console.WriteLine("***************");
+                input = int.Parse(Console.ReadLine());
+
+                if (input == 1)
+                {
+                    Console.WriteLine("What number would you like to input?");
+                    int num = int.Parse(Console.ReadLine());
+                    
+                    myStack.Push(num);
+                }
+
+                if (input == 2)
+                {
+                   int temp = myStack.Pop();
+                   Console.WriteLine("Number removed: " + temp);
+                }
+
+                if (input == 3)
+                {
+                    int temp = myStack.Peek();
+                    Console.WriteLine("Number at the top of the list: " + temp);
+                }
+
+                if (input == 4)
+                {
+                    myStack.Print();
+                }
+
+                if (input == 5)
+                {
+                    Console.WriteLine("What number would you like to find?");
+                    int val = int.Parse(Console.ReadLine());
+                    int num = myStack.Search(val);
+                    if (num == -1)
+                    {
+                        Console.WriteLine("Your Number Does not exists");
+                    }
+                      else      
+                    Console.WriteLine("your number is in the " + num + " place");
+
+
+                }
+
+            } while (input != 6);
+            Console.ReadKey();
+                      
+        }
+    }
+}
---
+++ Semester2/Stack_DylanAbeyta/Stack_DylanAbeyta/Stack.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_DylanAbeyta
+{
+    class Stack
+    {
+        List<int> holder = new List<int>();
+
+        public Stack()
+        {
+            
+        }
+
+        //Push
+        public void Push(int val)
+        {
+            holder.Add(val);
+        }
+
+        //Pop
+        public int Pop()
+        {
+            if (holder[0] == null)
+            {
+                Console.WriteLine("there are no numbers in the list. Press 1 and push a value.");
+            }
+                    
+            int temp = holder[holder.Count - 1];
+            holder.RemoveAt(holder.Count - 1);            
+            return temp;
+        }
+
+        //Peek
+        public int Peek()
+        {
+            int temp = holder[holder.Count - 1];
+            return temp;
+        }
+
+        //Print
+        public void Print()
+        {
+            for (int i = 0; i < holder.Count; i++)
+            {
+                Console.Write(holder[i] + ", ");
+            }
+            Console.WriteLine();
+        }
+        //Search
+        public int Search(int val)
+        {
+            
+            for (int i = 0; i < holder.Count; i++)
+            {
+                if (val == holder[i])
+                {                   
+                    return ((holder.Count - 1) - i);
+                    
+                }
+            }
+            
+            return -1;
+        }
+    }
+}
