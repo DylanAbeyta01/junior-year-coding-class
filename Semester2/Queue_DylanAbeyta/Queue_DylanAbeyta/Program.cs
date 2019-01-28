b6c7675f1d6d6a2e83bc196992ca15d1ef1775c3
DylanAbeyta <dylanabeyta@gmail.com>
2019-01-28 13:09:46 -0700

    i made a queue program that does the same thing as stack but instead it uses fifo

---
+++ Semester2/Queue_DylanAbeyta/Queue_DylanAbeyta/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_DylanAbeyta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue myQueue = new Queue();
+
+            int input;
+            do
+            {
+                Console.WriteLine("***************");
+                Console.WriteLine("** 1) EnQueue");
+                Console.WriteLine("** 2) DeQueue");
+                Console.WriteLine("** 3) Peek");
+                Console.WriteLine("** 4) Print");
+                Console.WriteLine("** 5) search");
+                Console.WriteLine("** 6) quit");
+                Console.WriteLine("***************");
+                input = int.Parse(Console.ReadLine());
+
+                if (input == 1)
+                {
+                    Console.WriteLine("What number would you like to input?");
+                    int num = int.Parse(Console.ReadLine());
+
+                    myQueue.EnQueue(num);
+                }
+
+                if (input == 2)
+                {
+                    int temp = myQueue.DeQueue();
+                    if (temp == -1)
+                    {
+                        Console.WriteLine("there are no numbers in the list. Press 1 and DeQueue a value.");
+                    }
+                   else         
+                    Console.WriteLine("Number removed: " + temp);
+                }
+
+                if (input == 3)
+                {
+                    int temp = myQueue.Peek();
+                    Console.WriteLine("Number at the top of the list: " + temp);
+                }
+
+                if (input == 4)
+                {
+                    myQueue.Print();
+                }
+
+                if (input == 5)
+                {
+                    Console.WriteLine("What number would you like to find?");
+                    int val = int.Parse(Console.ReadLine());
+                    int num = myQueue.Search(val);
+                    if (num == -1)
+                    {
+                        Console.WriteLine("Your Number Does not exists");
+                    }
+                    else
+                        Console.WriteLine("your number is in the " + num + " place");
+
+
+                }
+
+            } while (input != 6);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Semester2/Queue_DylanAbeyta/Queue_DylanAbeyta/Queue.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_DylanAbeyta
+{
+    class Queue
+    {
+        List<int> holder = new List<int>();
+
+        public Queue()
+        {
+
+        }
+
+        //Push
+        public void EnQueue(int val)
+        {            
+            holder.Insert(0, val);
+        }
+
+        //Pop
+        public int DeQueue()
+        {
+            int temp;
+            if (holder.Count == 0)
+            {
+                temp = -1;
+                return temp;
+            }
+            else if (holder.Count != 0)
+            {
+                temp = holder[holder.Count - 1];
+                holder.RemoveAt(holder.Count - 1);
+                return temp;
+            }
+            return 0;
+        }
+
+        //Peek
+        public int Peek()
+        {
+            int temp = holder[holder.Count - 1];
+            return temp;
+        }
+
+        //Print
+        public void Print()
+        {
+            for (int i = 0; i < holder.Count; i++)
+            {
+                Console.Write(holder[i] + ", ");
+            }
+            Console.WriteLine();
+        }
+        //Search
+        public int Search(int val)
+        {
+
+            for (int i = 0; i < holder.Count; i++)
+            {
+                if (val == holder[i])
+                {
+                    return ((holder.Count - 1) - i);
+
+                }
+            }
+
+            return -1;
+        }
+    }
+}
