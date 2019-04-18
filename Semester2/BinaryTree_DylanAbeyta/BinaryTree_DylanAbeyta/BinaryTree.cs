c10491b1928b7fc350265e2a599418104c590ef0
DylanAbeyta <dylanabeyta@gmail.com>
2019-04-18 14:48:44 -0600

    started working on making a binary tree i now have to start making the search part

---
+++ Semester2/BinaryTree_DylanAbeyta/BinaryTree_DylanAbeyta/BinaryTree.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree_DylanAbeyta
+{
+    class BinaryTree
+    {
+        private Node Root { get; set; }
+        public int Height;
+        public int Count;
+        
+        public void Print()
+        {
+            BinaryTreePrinter.printNode(Root);
+        }
+
+        private int CountRecursive(Node n)
+        {
+            throw new NotImplementedException();
+        }
+        public void Insert(char c)
+        {
+            if (Root == null)
+            {
+                Root = new Node(c);
+            }
+            else
+                Insert(Root, c);
+        }
+        private void Insert(Node n, char c)
+        {
+            if (c >=  n.Value && n.RightChild == null)
+            {
+                n.RightChild = new Node(c);
+                return;
+            }
+
+            if (c < n.Value && n.LeftChild == null)
+            {
+                n.LeftChild = new Node(c);
+                return;
+            }
+
+            if (c >= n.Value)
+            {
+                Insert(n.RightChild, c);
+            }
+
+            else if (c < n.Value)
+            {
+                Insert(n.LeftChild, c);
+            }
+
+
+        }
+        public bool Remove(char c)
+        {
+            throw new NotImplementedException();
+
+        }
+        private bool Remove(Node n, char c)
+        {
+            throw new NotImplementedException();
+
+        }
+        public bool Search(char c)
+        {
+
+            throw new NotImplementedException();
+
+        }
+        private bool Search(Node n, char c)
+        {
+            throw new NotImplementedException();
+        }
+        public void Balance()
+        {
+
+        }
+        public void PreOrderPrint()
+        {
+
+        }       
+        private void PreOrderPrint(Node n)
+        {
+
+        }             
+        public void InorderPrint()
+        {
+
+        }
+        private void InOrderPrint(Node n)
+        {
+
+        }
+        public void PostOrderPrint()
+        {
+
+        }
+        private void PostOrderprint(Node n)
+        {
+
+        }
+    }
+    
+}
---
+++ Semester2/BinaryTree_DylanAbeyta/BinaryTree_DylanAbeyta/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree_DylanAbeyta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree bt = new BinaryTree();
+            bt.Insert('a');
+            bt.Insert('o');
+            bt.Insert('c');
+            bt.Insert('p');
+            bt.Insert('f');
+            bt.Insert('t');
+            bt.Insert('j');
+            bt.Insert('k');
+            bt.Insert('i');
+            bt.Insert('s');
+            bt.Insert('q');
+            bt.Insert('w');
+            bt.Insert('z');
+            bt.Insert('x');
+            bt.Print();
+            Console.WriteLine("Enter a letter to search for");
+            char SLet = char.Parse(Console.ReadLine());
+            bt.Search(SLet);
+            Console.ReadKey();
+
+        }
+    }
+}
